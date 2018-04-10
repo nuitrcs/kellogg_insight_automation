543613ce014ed4f1c70f30760630cf99323b7e25
felavsky <elavsky@northwestern.edu>
2018-04-10 13:33:08 -0500

    updated result subheading again

--- scripts/mods/draw_jobs.js
+++ scripts/mods/draw_jobs.js
@@ -2,7 +2,7 @@ tilde.notices = {
 	"pre_pick": "Search to discover your job's risk of automation.<br>&nbsp;",
 	"option_chosen":"Calculating automation risk...<br>&nbsp;",
 	"finding_similars":"Finding similar jobs...<br>&nbsp;",
-	"subheading":"<b>Result</b>: This job + the 10 most similar jobs (based on<br>related skills) with their risk of automation."
+	"subheading":"<b>Result</b>: Average likelihood this job -and the 10 most<br>similar jobs- will be automated."
 }
 
 tilde.colors = ["#951F24","#C62026","#DD5524","#EF8F1E","#F2E74A","#A0F582"] //["#951F24","#F2E3E4"]
